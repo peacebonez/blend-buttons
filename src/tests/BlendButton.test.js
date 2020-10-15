c2c98e9fe127f8ed49a67cb10885a834087f49e5
peacebonez <kmpariso12@gmail.com>
2020-10-15 16:05:06 -0400

    finish enzyme tests

--- src/tests/BlendButton.test.js
+++ src/tests/BlendButton.test.js
@@ -1,27 +1,11 @@
 import React from "react";
-import { render } from "@testing-library/react";
 import Enzyme from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import { shallow, mount } from "enzyme";
 import ShallowRenderer from "react-test-renderer/shallow";
-import toJson from "enzyme-to-json";
 
-import App from "../App";
 import BlendButton from "../BlendButton";
 
 Enzyme.configure({ adapter: new Adapter() });
-
-describe("<App />", () => {
-  let wrapper;
-
-  it("renders without crashing", () => {
-    wrapper = shallow(<App />);
-  });
-
-  it("should render 7 <BlendButton /> components", () => {
-    expect(wrapper.find(BlendButton)).toHaveLength(7);
-  });
-});
 
 describe("<BlendButton />", () => {
   const renderer = new ShallowRenderer();
@@ -41,5 +25,23 @@ describe("<BlendButton />", () => {
     renderer.render(<BlendButton colorMain={"red"} />);
     const result = renderer.getRenderOutput();
     expect(result.props.style.background).toEqual("red");
+  });
+
+  it("borderRadius prop works", () => {
+    renderer.render(<BlendButton borderRadius={"20px"} />);
+    const result = renderer.getRenderOutput();
+    expect(result.props.style.borderRadius).toEqual("20px");
+  });
+
+  it("padding prop works", () => {
+    renderer.render(<BlendButton padding={"15px"} />);
+    const result = renderer.getRenderOutput();
+    expect(result.props.style.padding).toEqual("15px");
+  });
+
+  it("fontSize prop works", () => {
+    renderer.render(<BlendButton fontSize={"3vw"} />);
+    const result = renderer.getRenderOutput();
+    expect(result.props.style.fontSize).toEqual("3vw");
   });
 });
